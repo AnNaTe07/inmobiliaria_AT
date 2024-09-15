710748d146fa1b5cd15a3a74e317f783d42fe19f
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2024-09-15 16:23:06 -0300

    Usuario teminado, controles de vistas

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,47 +1,41 @@
-using System.ComponentModel.DataAnnotations; // Necesario para [Required] y [EmailAddress]
-using Microsoft.AspNetCore.Http; // Necesario para IFormFile (para el avatar)
+using System.ComponentModel.DataAnnotations;
 
-namespace inmobiliaria_AT.Models;
-
-
-public enum Rol
+namespace inmobiliaria_AT.Models
 {
-    Administrador = 1,
-    Empleado = 2,
-}
 
-public class Usuario
-{
-    public int Id { get; set; }
-    [Required]
-    public string Nombre { get; set; }
-    [Required]
-    public string Apellido { get; set; }
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
-    [Required]
-    public string PasswordHash { get; set; }
-    public string Salt { get; set; } = "";
-    public string Avatar { get; set; } = "";
-    public IFormFile? AvatarFile { get; set; }// para que pueda ser nulo
-    public Rol Rol { get; set; }
-    public string RolNombre => Rol.ToString();
+    public enum Roles
+    {
+        Administrador = 1,
+        Empleado = 2,
+    }
 
-    /* 
-        public static string ObtenerRol(int rolValue)
-        {
-            // Verifica si el valor del rol está en el rango válido de la enumeración
-            if (Enum.IsDefined(typeof(Rol), rolValue))
-            {
-                // Convierte el valor entero a la descripción del rol
-                Rol rol = (Rol)rolValue;
-                return rol.ToString();
-            }
-            else
-            {
-                // Retorna un valor predeterminado o un mensaje de error si el valor no es válido
-                return "Rol no válido";
-            }
-        } */
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "La contraseña es obligatoria.")]
+        //[DataType(DataType.Password)]
+        public string? Clave { get; set; }
+        public string Salt { get; set; } = "";
+        public string Avatar { get; set; } = "";
+        //[NotMapped]//Para EF
+        public IFormFile? AvatarFile { get; set; }// para que pueda ser nulo
+                                                  //[NotMapped]//Para EF
+                                                  //public byte[] AvatarFileContent { get; set; }
+                                                  //[NotMapped]//Para EF
+                                                  //public string AvatarFileName { get; set; }  
+        [Required(ErrorMessage = "El rol es obligatorio.")]
+        public Roles Rol { get; set; }
+        public string RolNombre => Rol.ToString();
+        // Propiedad auxiliar para gestionar la validación condicional
+        public bool Estado { get; set; }//para dar de baja el usuario
+    }
 }
